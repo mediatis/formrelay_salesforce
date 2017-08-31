95405cd4a3f5d9c6c027a8786ae057eff842d898
Daniel Bagel <db@dbagel.de>
2017-08-31 13:41:32 +0200

    Add composer.json

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,11 +29,11 @@ $EM_CONF[$_EXTKEY] = array(
     'clearCacheOnLoad' => 0,
     'lockType' => '',
     'author_company' => '',
-    'version' => '0.0.3',
+    'version' => '0.1.0',
     'constraints' => array(
         'depends' => array(
             'cms' => '',
-            'formrelay' => '0.0.6',
+            'formrelay' => '0.1.0',
         ),
         'conflicts' => array(
         ),
