e10eecefee17a5961bb566fb97fa4bf9edd266b5
Daniel Bagel <db@dbagel.de>
2018-07-23 13:24:27 +0200

    Update Javascript to write all encountered campaign numbers  to the cookie, not just the first one

--- Resources/Public/Javascript/campaigncookie.js
+++ Resources/Public/Javascript/campaigncookie.js
@@ -1,11 +1,23 @@
 jQuery(document).ready(function() {
-  // Set Cookie if current page has a campaign number and no cookie exists yet
+  // Set or update Cookie if current page has a campaign number
   if (typeof jQuery.cookie == 'function') {
+    
+    var unique = function(array) {
+      return $.grep(array, function(el, index) {
+        return index === $.inArray(el, array);
+      });
+    };
+    
     if (formrelay_salesforce_campaign_number !== undefined && formrelay_salesforce_campaign_number !== '') {
       var sfCookie = jQuery.cookie('sfCampaignNumber');
-      if (sfCookie == undefined || sfCookie == '') {
-        jQuery.cookie('sfCampaignNumber', formrelay_salesforce_campaign_number, { path: '/' });
+      if(sfCookie == null) {
+        sfCookie = formrelay_salesforce_campaign_number;
+      } else {
+        var cookieValues = sfCookie.split(',');
+        cookieValues.push(formrelay_salesforce_campaign_number);
+        sfCookie = unique(cookieValues);
       }
+      jQuery.cookie('sfCampaignNumber', sfCookie, { path: '/' });
     }
   }
 });
