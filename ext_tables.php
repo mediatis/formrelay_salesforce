75188798f160e78876ba7ef092e2caf9c22a84ed
Michael Vöhringer <voehringer@mediatis.de>
2018-09-14 16:05:30 +0200

    fix for debug option

--- ext_tables.php
+++ ext_tables.php
@@ -6,6 +6,6 @@ if (!defined('TYPO3_MODE')) {
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'FormRelay SalesForce');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-	'pages',
-	'EXT:formrelay_salesforce/Resources/Private/Language/locallang_csh_pages.xlf'
+    'pages',
+    'EXT:formrelay_salesforce/Resources/Private/Language/locallang_csh_pages.xlf'
 );
