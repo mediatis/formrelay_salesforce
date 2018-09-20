3c1f9d3ea69b6c482498db24efebab6cf10934f5
Daniel Bagel <db@dbagel.de>
2018-09-20 11:29:56 +0200

    Add TS constant to enable campaign number cookie deletion after sending

--- Classes/DataProvider/CampaignNumber.php
+++ Classes/DataProvider/CampaignNumber.php
@@ -1,12 +1,19 @@
 <?php
 namespace Mediatis\FormrelaySalesforce\DataProvider;
 
+use Mediatis\Formrelay\Utility\FormrelayUtility;
+
 class CampaignNumber implements \Mediatis\Formrelay\DataProviderInterface
 {
     public function addData(&$dataArray)
     {
         if ($_COOKIE['sfCampaignNumber'] != "") {
             $dataArray['sf_campaign_number'] = $_COOKIE['sfCampaignNumber'];
+			
+	        $settings = FormrelayUtility::loadPluginTS('tx_formrelay_salesforce');
+	        if($settings['campaignNumber.']['deleteCookieAfterSending'] == 1) {
+				setcookie('sfCampaignNumber', '', time() - 3600, '/');
+	        }
         }
     }
 }
