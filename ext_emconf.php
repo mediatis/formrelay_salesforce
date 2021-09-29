6b5a7b30c5080a76cdf96c7d33b2b5c8f2b4d02b
Meelis Karulin <karulin@mediatis.de>
2021-09-29 10:29:10 +0200

    11.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.0.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '10.4.0-11.5.99',
             'formrelay' => '>=4.0.0'
         ],
         'conflicts' => [
