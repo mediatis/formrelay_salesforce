834bbd611cb748d8b32a382618e65ec9eb55c29c
s-ude <ude@mediatis.de>
2021-10-14 01:38:09 +0200

    [FEATURE] Asynchronous Submissions
    * LMSCB-16: async form submissions updates
    
    * LMSCB-16: updated submission context handling
    
    * LMSCB-16: dependency updates

---
+++ Classes/Initialization.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Mediatis\FormrelaySalesforce;
+
+use FormRelay\Core\Service\RegistryInterface;
+use FormRelay\Request\Route\RequestRoute;
+use FormRelay\Salesforce\SalesforceInitialization;
+use Mediatis\FormrelaySalesforce\DataProvider\SfdcCampaignNumberDataProvider;
+
+class Initialization
+{
+    public function initialize(RegistryInterface $registry)
+    {
+        SalesforceInitialization::initialize($registry);
+        $registry->registerDataProvider(SfdcCampaignNumberDataProvider::class);
+    }
+}
