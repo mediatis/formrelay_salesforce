04579f844de4e6627b2dc16e28258c3df636a536
Meelis Karulin <mkarulin@gmail.com>
2022-02-21 14:12:53 +0100

    [TASK] Update for TYPO3 11 (#2)
    * Update for TYPO3 11
    * Drop TYPO3 9.5 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,14 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => '',
     'author_company' => 'Mediatis AG',
     'state' => 'alpha',
-    'internal' => '',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'clearCacheOnLoad' => 0,
     'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '10.4.0-11.5.99',
             'formrelay' => '>=5.0.0'
         ],
         'conflicts' => [
@@ -23,7 +19,5 @@ $EM_CONF[$_EXTKEY] = [
         'suggests' => [
             'news' => '*'
         ],
-    ],
-    'suggests' => [
     ],
 ];
