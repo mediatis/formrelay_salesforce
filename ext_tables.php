069837ab63d19bf3bfbeb494182b767725547b6c
Stephan Ude <ude@mediatis.de>
2022-10-24 10:35:41 +0200

    LMSTY11-115: CSH handling, cleanup

--- ext_tables.php
+++ ext_tables.php
@@ -4,5 +4,10 @@ defined('TYPO3') or die();
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
     'pages',
-    'EXT:formrelay_salesforce/Resources/Private/Language/locallang_csh_pages.xlf'
+    'EXT:formrelay_salesforce/Resources/Private/Language/locallang_csh_pages_and_tx_news_domain_model_news.xlf'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+    'tx_news_domain_model_news',
+    'EXT:formrelay_salesforce/Resources/Private/Language/locallang_csh_pages_and_tx_news_domain_model_news.xlf'
 );
